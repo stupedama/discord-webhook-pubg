469b21bc6d565eea613ed6b52df8771cb443ec83
Fredrik <fredrik@ffj.no>
2019-02-22 14:01:29 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# please add your id and token to this file
+id = ""
+token = ""
+
+
+# Name of the webhook/bot
+bot_name = "PUBG Bot"
+
+
+# discord-webhook-pubg version
+version = "0.4-krabbetein"
